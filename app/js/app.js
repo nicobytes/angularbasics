e1c9eae1ccb2ef9d4094d4cfee5305eb0edec978
Nicolas Molina <nicolas.molina.monroy@3dementes.com>
2015-09-07 20:48:08 -0500

    Update project with angular 1.4.5

--- app/js/app.js
+++ app/js/app.js
@@ -1,47 +1,44 @@
 (function() {
-    "use strict";
+  "use strict";
 
-    var app = angular.module("HelloWorldApp", [], function($routeProvider, $locationProvider) {
-        $routeProvider.when("/area", {
-            templateUrl: 'templates/area.html'
-        });
+  var app = angular.module("HelloWorldApp", [
+    'ngRoute'
+  ]);
 
-        $routeProvider.when("/perimeter", {
-            templateUrl: 'templates/perimeter.html'
-        });
+  app.config(function($routeProvider, $locationProvider) {
 
-        $locationProvider.html5Mode(false);
+    $routeProvider.when("/area", {
+      templateUrl: 'templates/area.html'
     });
 
-    app.controller("HelloWorldController", [ "$scope", "$route", "$routeParams", "$location", function($scope, $route, $routeParams, $location) {
+    $routeProvider.when("/perimeter", {
+      templateUrl: 'templates/perimeter.html'
+    });
 
-        $scope.$route = $route;
-        $scope.$location = $location;
-        $scope.$routeParams = $routeParams;
-        $scope.numberError = false;
+    $locationProvider.html5Mode(false);
+  });
 
-        $scope.circleArea = function() {
-            console.log($scope);
-            return Math.PI * Math.pow($scope.number, 2);
-        };
+  app.controller("HelloWorldController",[
+    '$scope',
+    function($scope){
 
-        $scope.circleArea = function() {
-            return Math.PI * Math.pow($scope.number, 2);
-        };
+      $scope.circleArea = function() {
+        return Math.PI * Math.pow($scope.number, 2);
+      };
 
-        $scope.circlePerimeter = function() {
-            return 2 * Math.PI * $scope.number;
-        };
+      $scope.circlePerimeter = function() {
+        return 2 * Math.PI * $scope.number;
+      };
 
-        $scope.$watch("number", function(newValue, oldValue) {
-            if (newValue === undefined) return;
+      $scope.$watch("number", function( newValue, oldValue ) {
+        if ( newValue === undefined ) return;
 
-            if (isNaN(newValue)) {
-                $scope.numberError = true;
-            } else {
-                $scope.numberError = false;
-            }
-        });
-    }]);
-
+        if ( isNaN( newValue ) ) {
+          $scope.numberError = true;
+        } else {
+          $scope.numberError = false;
+        }
+      });
+    }
+  ]);
 }());
